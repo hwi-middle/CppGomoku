3d9041cc750a879c124aaf3a599efdfa974d5abc
hwi-middle <gnlwnd5084@gmail.com>
2021-02-01 05:07:46 +0900

    구현에 필요한 열거형 추가

--- Gomoku/Gomoku.h
+++ Gomoku/Gomoku.h
@@ -2,7 +2,20 @@
 #include <iostream>
 #include <cassert>
 
-enum class eInputKey	//유효한 입력 키들
+enum class eRules	//진행중인 게임의 룰
+{
+	FREE,
+	STANDARD,
+	RENJU
+};
+
+enum class eStones	//각 칸에 올 수 있는 돌
+{
+	BLACK,
+	WHITE
+};
+
+enum class eInputKeys	//유효한 입력 키들
 {
 	ARROW_LEFT,
 	ARROW_RIGHT,
